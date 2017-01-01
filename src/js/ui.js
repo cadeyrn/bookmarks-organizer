1f8a19b628193a8caf1f7a7c85387af3362e33af
Sören Hentzschel <kontakt@agenedia.com>
2017-01-01 16:12:58 +0100

    Feature: Lesezeichen entfernen

--- src/js/ui.js
+++ src/js/ui.js
@@ -12,18 +12,33 @@ const ui = {
       let bookmark = response.bookmark;
       let template = document.getElementById('result-template').content.cloneNode(true);
 
+      template.querySelector('.wrapper').parentNode.id = bookmark.id;
       template.querySelector('.name').innerText = bookmark.title;
       template.querySelector('.url').innerText = bookmark.url;
+      template.querySelector('.parentId').innerText = 'Parent-ID: ' + bookmark.parentId;
 
       if (bookmark.status !== 0) {
-        template.querySelector('.status').innerText = bookmark.status;
+        template.querySelector('.status').innerText = 'Status: ' + bookmark.status;
       }
 
+      template.querySelector('.remove').setAttribute('data-id', bookmark.id);
+
       elResults.appendChild(template);
+    }
+  },
+
+  removeBookmark : function (e) {
+    e.preventDefault();
+
+    if (e.target.tagName.toLowerCase() === 'a') {
+      let bookmarkId = e.target.getAttribute('data-id');
+      document.getElementById(bookmarkId).style.display = 'none';
+      browser.bookmarks.remove(bookmarkId);
     }
   }
 };
 
 document.querySelector('button').addEventListener('click', ui.execute);
+document.querySelector('body').addEventListener('click', ui.removeBookmark);
 
 browser.runtime.onMessage.addListener(ui.handleResponse);
