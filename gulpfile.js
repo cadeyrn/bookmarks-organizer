c0fa92379b5c4b133d3c1f5432c2026cdec48af7
Sören Hentzschel <kontakt@agenedia.com>
2017-02-25 10:18:24 +0100

    Verwendung von stylelint vorbereitet

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,17 @@
 
 const gulp = require('gulp');
 const gulpEslint = require('gulp-eslint');
+const gulpStylelint = require('gulp-stylelint');
 
 gulp.task('lint-js', () => gulp.src(['**/*.js', '!node_modules/**'])
   .pipe(gulpEslint({ configFile : '.eslintrc.json' }))
   .pipe(gulpEslint.format()));
+
+gulp.task('lint-css', () => gulp.src(['**/*.css'])
+  .pipe(gulpStylelint({
+    failAfterError : false,
+    reporters : [{
+      formatter : 'string',
+      console : true
+    }]
+  })));
