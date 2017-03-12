99805736308793fafa59c21bb8653055d0e7450e
Sören Hentzschel <kontakt@agenedia.com>
2017-03-12 13:15:23 +0100

    Unterstützung für JSDoc hinzugefügt

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,19 @@ const gulp = require('gulp');
 const gulpEslint = require('gulp-eslint');
 const gulpHtmllint = require('gulp-html-lint');
 const gulpStylelint = require('gulp-stylelint');
+const jsdoc = require('gulp-jsdoc3');
 
-gulp.task('lint-html', () => gulp.src(['**/*.html', '!node_modules/**'])
+gulp.task('lint-html', () => gulp.src(['./src/html/*.html'])
   .pipe(gulpHtmllint({ htmllintrc : '.htmllintrc.json' }))
   .pipe(gulpHtmllint.format())
 );
 
-gulp.task('lint-js', () => gulp.src(['**/*.js', '!node_modules/**'])
+gulp.task('lint-js', () => gulp.src(['gulpfile.js', './src/js/*.js'])
   .pipe(gulpEslint({ configFile : '.eslintrc.json' }))
   .pipe(gulpEslint.format())
 );
 
-gulp.task('lint-css', () => gulp.src(['**/*.css'])
+gulp.task('lint-css', () => gulp.src(['./src/css/*.css'])
   .pipe(gulpStylelint({
     failAfterError : false,
     reporters : [{
@@ -23,4 +24,9 @@ gulp.task('lint-css', () => gulp.src(['**/*.css'])
       console : true
     }]
   }))
+);
+
+const jsdocsConfig = require('./jsdoc.json');
+gulp.task('docs', () => gulp.src(['CHANGELOG.md', 'README.md', './src/js/*.js'], { read : false })
+  .pipe(jsdoc(jsdocsConfig))
 );
