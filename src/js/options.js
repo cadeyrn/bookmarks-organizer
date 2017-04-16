2bf3a2022511178de69d8b5d1d19f9d890619788
Sören Hentzschel <kontakt@agenedia.com>
2017-04-16 08:53:19 +0200

    Follow-up: Variable „debug_enabled“ in camelCase geändert

--- src/js/options.js
+++ src/js/options.js
@@ -5,12 +5,12 @@ const elDebugMode = document.getElementById('debug-mode');
 const options = {
   async load () {
     const option = await browser.storage.local.get({ debug_enabled : false });
-    elDebugMode.checked = option.debug_enabled;
+    elDebugMode.checked = option.debugEnabled;
   }
 };
 
 document.addEventListener('DOMContentLoaded', options.load);
 
 elDebugMode.addEventListener('change', (e) => {
-  browser.storage.local.set({ debug_enabled : e.target.checked });
+  browser.storage.local.set({ debugEnabled : e.target.checked });
 });
