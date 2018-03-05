da40f3c4c34e23333b0f5dba879be3d5697bfde5
Sören Hentzschel <cadeyrn@ymail.com>
2018-03-05 09:16:54 +0100

    fixed typo

--- src/js/core/ui.js
+++ src/js/core/ui.js
@@ -718,7 +718,7 @@ const ui = {
   },
 
   /**
-   * Fired when of the action buttons is clicked.
+   * Fired when one of the action buttons is clicked.
    *
    * @param {MouseEvent} e - event
    *
